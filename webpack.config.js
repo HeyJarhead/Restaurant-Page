dc4da35adcb17b7837a752ed79e4176fdf721f30
Jarhead <hdog358@aol.com>
2023-02-20 13:16:48 -0800

    Add webpack, directories and files

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = requre('path');
+
+module.exports = {
+    entry: './src/index.js', 
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname,'dist'),
+    },
+};
