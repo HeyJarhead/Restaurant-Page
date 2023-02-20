77ef7334263306fb3bbfc714783bc1458017964a
Jarhead <hdog358@aol.com>
2023-02-20 14:58:06 -0800

    add tab menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 
-
 import { loadHeader, loadImage, loadDescription  } from './main-load';
+
+// Create header
+const header = document.createElement('header');
+ document.body.append(header);
+
+// create the navigation container
+const navContainer = document.createElement("nav");
+header.appendChild(navContainer);
+
+// create ul of tabs
+const tabs = document.createElement('ul');
+const home = document.createTextNode("Home");
+const menu = document.createTextNode("Menu");
+const contact = document.createTextNode('Contact');
+navContainer.appendChild(tabs);
+tabs.appendChild(home);
+tabs.appendChild(menu);
+tabs.appendChild(contact);
+
+
 const container = document.getElementById("container");
 container.appendChild(loadHeader());
 container.appendChild(loadImage());
