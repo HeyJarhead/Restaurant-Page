bc6db4e7b8c3fabc761fc6bbfc1ae5e5bc199ef6
Jarhead <hdog358@aol.com>
2023-02-20 14:22:40 -0800

    add fix to functions

--- src/main-load.js
+++ src/main-load.js
@@ -1,16 +1,23 @@
 import Icon from './img/sushi.jpg';
 function loadHeader()  { 
     const header = document.createElement('h1');
-    header.innerHTML = "Galatic Sushi";
+    header.textContent = "Galatic Sushi";
     return header;
 }
+
 function loadImage() {
     const sushiPic = new Image();
     sushiPic.src = Icon;
     return sushiPic;
 }
+
 function loadDescription() {
     const description = document.createElement('div');
     description.innerHTML = "This sushi will send your taste buds out of this world!"
     return description;
 }
+export {
+    loadHeader,
+    loadImage,
+    loadDescription
+};
