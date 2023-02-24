f32a9ef92a1246edf5e7ba0eaab7e9c4cbc89db4
Jarhead <hdog358@aol.com>
2023-02-24 13:05:10 -0800

    move tab creation to main-load

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 
-import { loadHeader, loadImage, loadDescription  } from './main-load';
+import { loadHeader, loadImage, loadDescription, createTabs  } from './main-load';
+import { homeTab, homeClicked } from './home-tab';
 
 // Create header
-const header = document.createElement('header');
- document.body.append(header);
-
-// create the navigation container
-const navContainer = document.createElement("nav");
-header.appendChild(navContainer);
-
-// create ul of tabs
-const tabs = document.createElement('ul');
-const home = document.createTextNode("Home");
-const menu = document.createTextNode("Menu");
-const contact = document.createTextNode('Contact');
-navContainer.appendChild(tabs);
-tabs.appendChild(home);
-tabs.appendChild(menu);
-tabs.appendChild(contact);
-
+createTabs();
 
 const container = document.getElementById("container");
 container.appendChild(loadHeader());
 container.appendChild(loadImage());
 container.appendChild(loadDescription());
+
+
+// add dom credits for stuff taken
+/// Add tab switching functions to be called 
+// 
