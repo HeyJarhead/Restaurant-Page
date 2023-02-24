961b14b8491ddc409f110087181d2e6a33a699c7
Jarhead <hdog358@aol.com>
2023-02-24 15:00:44 -0800

    fix unwanted dom results

--- src/tab-module.js
+++ src/tab-module.js
@@ -1,6 +1,71 @@
+import './style.css';
 function homeTab() {
     const homeContainer = document.createElement('div');
-    homeContainer.className = "show";
+    homeContainer.id = "show-home";
+        const homeStoryHeader = document.createElement('h2');
+        homeStoryHeader.className = "header";
+        homeStoryHeader.textContent = "What is Galatic Sushi?";
+        homeContainer.appendChild(homeStoryHeader);
+            const homeStory = document.createElement('p');
+            homeStory.textContent = "Galatic Sushi is the best place for sushi or any other type of fish rolls (also known as Maki.)" +
+            " Mac Lucas, our owner, was wandering around Earth wondering if there was any good sushi restaurants around..." +
+            " unfortunately none were good enough for him. There was no hope. Mac Lucas looked up into the sky and realized" + 
+            " there was hope... Mac Lucas, he had to be the hope and thus Galatic Sushi was born.";
+            homeContainer.appendChild(homeStory);
+            //homeStoryHeader.appendChild(homeStory);
+        const pitchHeader = document.createElement("h2");
+        pitchHeader.className = "header";
+        pitchHeader.textContent = "Why eat here over other sushi places?";
+        homeContainer.appendChild(pitchHeader)
+            const pitch = document.createElement('p');
+            pitch.textContent = "The Galatic Sushi experience is not like any other experience. Here, you can have the atmoshpere of" +
+            " being in a space lounge while having a top of the line sushi dish at an affordable price."
+            homeContainer.appendChild(pitch);
+            //pitchHeader.appendChild(pitch);
+    // homeContainer.appendChild(homeStoryHeader);
+    // homeContainer.appendChild(pitchHeader);
+    return homeContainer;
+}
+/// html will change class name to hidden on the specific tab
+/// css will hide any class name with hidden
+/// js will have the change listener 
+function homeClicked() {
+    const homeTab = document.getElementById("home");
+    const show = document.getElementById("test");
+    const contactTab = document.getElementById("contact");
+    const menuTab = document.getElementById("menu");
+    homeTab.className = "clicked";
+    contactTab.className = "unclicked";
+    menuTab.className = "unclicked";
+    show.replaceWith(test2());
+}
+function contactClicked() {
+    const homeTab = document.getElementById("home");
+    const show = document.getElementById("show-home");
+    const contactTab = document.getElementById("contact");
+    const menuTab = document.getElementById("menu");
+    homeTab.className = "unclicked";
+    contactTab.className = "clicked";
+    menuTab.className = "unclicked";
+    show.replaceWith(test());
+}
+function menuClicked() {
+    const homeTab = document.getElementById("home");
+    const contactTab = document.getElementById("contact");
+    const menuTab = document.getElementById("menu");
+    homeTab.className = "unclicked";
+    contactTab.className = "unclicked";
+    menuTab.className = "clicked";
+}
+function test(){
+    const test = document.createElement('div');
+    test.id = "test";
+    test.textContent = 'test';
+    return test;
+}
+function test2(){
+    const homeContainer = document.createElement('div');
+    homeContainer.id = "show-home";
         const homeStoryHeader = document.createElement('h2');
         homeStoryHeader.textContent = "What is Galatic Sushi?";
             const homeStory = document.createElement('p');
@@ -19,15 +84,10 @@ function homeTab() {
     homeContainer.appendChild(pitchHeader);
     return homeContainer;
 }
-function homeClicked() {
-    const homeTab = document.getElementById("home");
-    const contactTab = document.getElementById("contact");
-    const menuTab = document.getElementById("menu");
-    homeTab.className = "";
-    contactTab.className = "hidden";
-    menuTab.className = "hidden";
-}
+
 export {
     homeTab,
-    homeClicked
+    homeClicked,
+    contactClicked,
+    menuClicked
 }
