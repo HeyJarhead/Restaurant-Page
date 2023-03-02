f17cc5117e8bb0cf113b79d97e9c144fba716933
Jarhead <hdog358@aol.com>
2023-03-02 11:30:13 -0800

    add style to menu items

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 
 import { loadHeader, loadImage, loadDescription, createTabs  } from './main-load';
-import { homeTab, homeClicked } from './tab-module';
 import './style.css';
 
-// Create header
+// create header
 createTabs();
 
+// appends children from hard coded html DOM
 const container = document.getElementById("container");
 container.appendChild(loadHeader());
 container.appendChild(loadImage());
 container.appendChild(loadDescription());
-
-/// Tab switch logic goes here to hide/ show the contents of the tab
-// add dom credits for stuff taken
-/// Add tab switching functions to be called 
-// 
